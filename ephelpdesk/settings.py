4af5eb4065a70683fef782990053cbb54f843f48
Marc-Andre Lemburg <mal@lemburg.com>
2015-03-02 11:04:53 +0100

    Explicitly disable the password change setting

--- ephelpdesk/settings.py
+++ ephelpdesk/settings.py
@@ -107,7 +107,7 @@ HELPDESK_KB_ENABLED = False
 # XXX There's a but in the app which causes a:
 #     NoReverseMatch at /dashboard/ - Reverse for 'auth_password_change' errors
 #     when enabling this setting!
-#HELPDESK_SHOW_CHANGE_PASSWORD = True
+HELPDESK_SHOW_CHANGE_PASSWORD = False
 
 # Helpdesk email subject
 HELPDESK_EMAIL_SUBJECT_TEMPLATE = "{{ ticket.title|safe }} %(subject)s {{ ticket.ticket }}"
