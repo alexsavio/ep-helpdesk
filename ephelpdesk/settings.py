dfaa99653ca6ebae2d6057bd7964bfaf3a3689f9
Marc-Andre Lemburg <mal@lemburg.com>
2017-01-11 16:23:04 +0100

    Clean up the settings.py file a bit and add comments.

--- ephelpdesk/settings.py
+++ ephelpdesk/settings.py
@@ -2,19 +2,17 @@
 Django settings for ephelpdesk project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
+https://docs.djangoproject.com/en/1.8/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
+https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+### General purpose Django settings
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("KEY") 
@@ -22,8 +20,9 @@ SECRET_KEY = os.environ.get("KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "").lower() in ("true", "1")
 # Force DEBUG to True, since the helpdesk doesn't run with DEBUG = False; see #2
-DEBUG = True
+#DEBUG = True
 
+# Hosts allowed to contact us
 ALLOWED_HOSTS = ['*']
 
 # Site ID setting
@@ -61,7 +60,7 @@ WSGI_APPLICATION = 'ephelpdesk.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -70,6 +69,7 @@ DATABASES = {
     }
 }
 
+# Templating engine
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [
@@ -97,28 +97,22 @@ TEMPLATES = [{
 }]
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
+# EMail settings
 #EMAIL_HOST = os.environ.get("EMAIL_HOST") 
 #EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER") 
 #EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD") 
